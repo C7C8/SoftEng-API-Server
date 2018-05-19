785e7ff60ba490e1929f4f867e48d6592ac47a98
Sourec <tothemyers@gmail.com>
2018-05-19 19:58:34 -0400

    Add some early auth stuff...

--- app.py
+++ app.py
@@ -1,30 +1,63 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from json import dumps
-from flask import jsonify
-import bcrypt
+from database import APIDatabase, UsernameException
 
 app = Flask(__name__)
 api = Api(app)
+db = APIDatabase()
 
 
-class Login(Resource):
+class Auth(Resource):
 	def get(self):
-		if len(request.get_data()) == 0:
+		"""Get a token"""
+		request.get_data()
+		if len(request.args) == 0:
 			return None, 400
+
+		username = request.args["username"]
+		password = request.args["password"]
+		try:
+			if db.authenticate(username, password):
+				return "Success", 200
+			else:
+				return "Bad login", 401
+		except UsernameException:
+			return "Bad username", 401
+
+	def post(self):
+		"""Register new user"""
+		request.get_data()
+		if len(request.json) == 0:
+			return None, 400
+
 		username = request.json["username"]
 		password = request.json["password"]
-		hashed = bcrypt.hashpw("test", bcrypt.gensalt())
-		print("Username: " + username)
-		print("Password: " + password)
-		print("Hashedpw: " + hashed)
-		if bcrypt.checkpw(password, hashed):
-			print("Password matches!")
-		else:
-			print("Password doesn't match!")
+		term = request.json["term"]
+		year = request.json["year"]
+		team = request.json["team"]
+		try:
+			db.registerUser(username, password, term, year, team)
+		except UsernameException:
+			return "Username exists", 403
+
+	def delete(self):
+		request.get_data()
+		if len(request.args) == 0:
+			return None, 400
+
+		username = request.args["username"]
+		password = request.args["password"]
+		try:
+			if db.authenticate(username, password):
+				db.deleteUser(username)
+				return "Success", 200
+			else:
+				return "Bad login", 401
+		except UsernameException:
+			return "Bad username", 401
 
 
-api.add_resource(Login, "/login")
+api.add_resource(Auth, "/auth")
 
 if __name__ == "__main__":
 	app.run(port="5000")
---
+++ database.py
@@ -0,0 +1,52 @@
+import pymysql
+from bcrypt import hashpw, gensalt, checkpw
+from sqlalchemy.util import NoneType
+
+
+class UsernameException(Exception):
+	pass
+
+
+class APIDatabase:
+	def __init__(self):
+		self.connection = pymysql.connect(
+			host="localhost",
+			port=3306,
+			user="list-api-service",
+			password="pass",
+			database="apilist"
+		)
+		self.cursor = self.connection.cursor()
+
+	def __del__(self):
+		self.cursor.close()
+		self.connection.close()
+
+	def registerUser(self, username, password, term, year, team):
+		"""Add a user to the database, if they don't already exist."""
+		sql = "SELECT username FROM users WHERE username=%s"
+		self.cursor.execute(sql, username)
+		if len(self.cursor.fetchall()) > 0:
+			raise UsernameException("User already exists in database")
+
+		sql = "INSERT INTO users (username, password, term, year, team) VALUES(%s, %s, %s, %s, %s)"
+		self.cursor.execute(sql, (username, hashpw(password, gensalt()), term, year, team))
+		self.connection.commit()
+
+	def deleteUser(self, username):
+		sql = "DELETE FROM users WHERE username=%s"
+		self.cursor.execute(sql, username)
+		self.connection.commit()
+
+	def authenticate(self, username, password):
+		"""Authenticate username/password combo, just returns true/false"""
+		sql = "SELECT password FROM users WHERE username=%s"
+		self.cursor.execute(sql, username)
+		res = self.cursor.fetchone()
+		if type(res) is NoneType:
+			raise UsernameException("Couldn't find username")
+
+		return checkpw(password, res[0])
+
+	def getToken(self, username):
+		pass
