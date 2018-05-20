14aa95132d527c9da45b1ce3f0f64b2612931705
Sourec <tothemyers@gmail.com>
2018-05-20 15:45:08 -0400

    Add primitive API adder

--- apiDB.py
+++ apiDB.py
@@ -1,4 +1,5 @@
 import pymysql
+import uuid
 from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.util import NoneType
 
@@ -31,6 +32,7 @@ class APIDatabase:
 		return True
 
 	def deleteUser(self, username):
+		"""Delete a user from the database"""
 		sql = "DELETE FROM users WHERE username=%s"
 		self.cursor.execute(sql, username)
 		self.connection.commit()
@@ -44,3 +46,19 @@ class APIDatabase:
 			return False
 
 		return checkpw(password, res[0])
+
+	def createAPI(self, username, name, contact, description):
+		"""Create an API entry. Returns the API's ID"""
+
+		# Create base entry in master API table
+		sql = "INSERT INTO api (id, name, version, contact, description, creator) VALUES(%s, %s, '1.0.0', %s, %s, %s)"
+		id = str(uuid.uuid4())
+		self.cursor.execute(sql, (id, name, contact, description, username))
+		self.connection.commit()
+
+		# Create single entry in version table
+		sql = "INSERT INTO version(apiId, info) VALUES (%s, %s)"
+		self.cursor.execute(sql, (id, "1.0.0 Initial release"))
+		# TODO Create function for updating JSON file
+
+		return id
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, JWTManager
+from flask_restful.reqparse import RequestParser
+from flask_restful import Resource, Api
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, \
+	get_jwt_identity, JWTManager
 import datetime
 from apiDB import APIDatabase
 
@@ -15,29 +17,31 @@ jwt = JWTManager(app)
 class Auth(Resource):
 	def get(self):
 		"""Login, return a token"""
-		parser = reqparse.RequestParser()
+		parser = RequestParser()
 		parser.add_argument("username", help="Must provide username to log in with", type=str, required=True)
 		parser.add_argument("password", help="Must provide password to log in with", type=str, required=True)
 		args = parser.parse_args()
 		if not db.authenticate(args["username"], args["password"]):
 			return {"message": "Incorrect username or password"}, 401
-		expires = datetime.timedelta(minutes=20)
+		expires = datetime.timedelta(days=20)  # TODO: Change to 1 hour
 		atoken = create_access_token(args["username"], expires_delta=expires)
 		rtoken = create_refresh_token(args["username"], expires_delta=expires)
 		return {
-			"message": "Logged in as {}".format(args["username"]),
-			"access_token": atoken,
-			"refresh_token": rtoken
-		}, 200
+				   "message": "Logged in as {}".format(args["username"]),
+				   "access_token": atoken,
+				   "refresh_token": rtoken
+			   }, 200
 
 	def post(self):
 		"""Register new user"""
-		parser = reqparse.RequestParser()
+		parser = RequestParser()
 		parser.add_argument("username", help="Must provide username to register", required=True, type=str)
 		parser.add_argument("password", help="Must provide password to set for new user", required=True, type=str)
-		parser.add_argument("term", help="Provide term (A,B,C,D) that user is registering from", required=True, type=str)
+		parser.add_argument("term", help="Provide term (A,B,C,D) that user is registering from", required=True,
+							type=str)
 		parser.add_argument("year", help="Provide year that user is registering from", required=True, type=int)
-		parser.add_argument("team", help="Provide letter of team that user is registering from", required=True, type=str)
+		parser.add_argument("team", help="Provide letter of team that user is registering from", required=True,
+							type=str)
 
 		args = parser.parse_args()
 		username = args["username"]
@@ -51,7 +55,7 @@ class Auth(Resource):
 
 	def delete(self):
 		"""Delete user, requires password as confirmation"""
-		parser = reqparse.RequestParser()
+		parser = RequestParser()
 		parser.add_argument("username", help="Must provide username to log in with", required=True, type=str)
 		parser.add_argument("password", help="Must provide password to log in with", required=True, type=str)
 		args = parser.parse_args()
@@ -62,7 +66,22 @@ class Auth(Resource):
 			return {"message": "Incorrect username or password"}, 401
 
 
-api.add_resource(Auth, "/auth")
+class APIList(Resource):
+
+	@jwt_required
+	def post(self):
+		"""Create/update API entry"""
+		parser = RequestParser()
+		parser.add_argument("name", help="Provide name of API", required=True, type=str)
+		parser.add_argument("version", help="Provide API version string", required=True, type=str)
+		parser.add_argument("contact", help="Provide email address of API maintainer", required=True, type=str)
+		parser.add_argument("description", help="Provide long form description of API", required=False, type=str)
+		args = parser.parse_args()
+		id = db.createAPI(get_jwt_identity(), args["name"], args["contact"], args["description"])
+		return {"message": "Created {}".format(args["name"]), "id": id}, 200
+
+api.add_resource(Auth, "/api/auth")
+api.add_resource(APIList, "/api/list")
 
 if __name__ == "__main__":
 	app.run(port="5000")
