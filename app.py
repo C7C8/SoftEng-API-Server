d8c1e6e54ec29c7520ad5f8ea09742ef9d90ef4e
Sourec <tothemyers@gmail.com>
2018-05-20 13:46:19 -0400

    Make argument types explicit

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ class Auth(Resource):
 	def get(self):
 		"""Login, return a token"""
 		parser = reqparse.RequestParser()
-		parser.add_argument("username", help="Must provide username to log in with", required=True)
-		parser.add_argument("password", help="Must provide password to log in with", required=True)
+		parser.add_argument("username", help="Must provide username to log in with", type=str, required=True)
+		parser.add_argument("password", help="Must provide password to log in with", type=str, required=True)
 		args = parser.parse_args()
 		try:
 			if db.authenticate(args["username"], args["password"]):
@@ -25,11 +25,11 @@ class Auth(Resource):
 	def post(self):
 		"""Register new user"""
 		parser = reqparse.RequestParser()
-		parser.add_argument("username", help="Must provide username to register", required=True)
-		parser.add_argument("password", help="Must provide password to set for new user", required=True)
-		parser.add_argument("term", help="Provide term (A,B,C,D) that user is registering from", required=True)
-		parser.add_argument("year", help="Provide year that user is registering from", required=True)
-		parser.add_argument("team", help="Provide letter of team that user is registering from", required=True)
+		parser.add_argument("username", help="Must provide username to register", required=True, type=str)
+		parser.add_argument("password", help="Must provide password to set for new user", required=True, type=str)
+		parser.add_argument("term", help="Provide term (A,B,C,D) that user is registering from", required=True, type=str)
+		parser.add_argument("year", help="Provide year that user is registering from", required=True, type=int)
+		parser.add_argument("team", help="Provide letter of team that user is registering from", required=True, type=str)
 
 		args = parser.parse_args()
 		username = args["username"]
@@ -46,8 +46,8 @@ class Auth(Resource):
 	def delete(self):
 		"""Delete user, requires password as confirmation"""
 		parser = reqparse.RequestParser()
-		parser.add_argument("username", help="Must provide username to log in with", required=True)
-		parser.add_argument("password", help="Must provide password to log in with", required=True)
+		parser.add_argument("username", help="Must provide username to log in with", required=True, type=str)
+		parser.add_argument("password", help="Must provide password to log in with", required=True, type=str)
 		args = parser.parse_args()
 		try:
 			if db.authenticate(args["username"], args["password"]):
